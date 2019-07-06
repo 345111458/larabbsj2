7d80e933f5715a55a350d97baa793034671aff0c
vagrant <vagrant@homestead>
2019-07-06 16:10:51 +0000

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,15 +2,24 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+    	$user_id = User::all()->pluck('id')->toArray();
+
+    	$category_id = Category::all()->pluck('id')->toArray();
+
+    	$faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)->times(500)->make()->each(function ($topic, $index) use ($user_id,$category_id,$faker) {
+            
+            $topic->user_id = $faker->randomElement($user_id);
+
+            $topic->category_id = $faker->randomElement($category_id);
         });
 
         Topic::insert($topics->toArray());
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	// 头像假数据
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+
+    	$faker = app(Faker\Generator::class);
+        //
+        $users = factory(User::class)->times(30)->make()->each(function($user) use ($faker,$avatars){
+
+        	$user->avatar = $faker->randomElement($avatars);
+        });
+
+
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summ345111458@qq.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+
+    }
+}
